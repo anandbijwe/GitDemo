582a5baa61229c5461db3cc3b08a83865c8aaf01
anandbijwe <anand.bijwe@gmail.com>
2020-04-08 16:35:33 +0530

    first commit

---
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,50 @@
+package com.crm.qa.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.crm.qa.util.TestUtil;
+
+public class TestBase {
+	
+public	static WebDriver driver;
+public	static Properties prop;
+		public TestBase(){
+			
+			try {
+				prop=new Properties();
+				FileInputStream ip=new FileInputStream("C:\\Users\\abijwe\\FreeCRM\\FreeCRMTest\\src\\main\\java\\com\\crm\\config\\config.properties");
+				
+					prop.load(ip);
+				} catch (FileNotFoundException e){
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();		
+				}
+			
+		}
+		
+		public static void initialization() {
+		String browserName	= prop.getProperty("browser");
+		if(browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\drivers\\driver\\02142020\\chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.Page_Load_Timout,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		driver.get(prop.getProperty("url"));
+			
+		}
+		
+		
+}
---
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -0,0 +1,7 @@
+package com.crm.qa.util;
+
+public class TestUtil {
+
+		public static long Page_Load_Timout=20;
+		public static long IMPLICIT_WAIT=10;
+}
